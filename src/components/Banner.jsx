5cc7c38db76b6fd1c6c322e0ac38b4543650e042
unknown <ezequiel.ledesma026@gmail.com>
2025-05-25 15:48:40 -0300

    se pone imagen de forma local

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import bannerImg from '../img/Pokémon_Trading_Card_Game_logo.svg.png';
 
 const BannerPokemon = () => (
     <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '1rem' }}>
         <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Pok%C3%A9mon_Trading_Card_Game_logo.svg/312px-Pok%C3%A9mon_Trading_Card_Game_logo.svg.png?20141222060945"
-        alt="Banner Pokemon"
-        style={{
-        width: '100%',
-        maxWidth: '250px',
-        height: 'auto',
-        objectFit: 'contain',
-        borderRadius: '12px',
-        boxShadow: '0 4px 16px rgba(0,0,0,0.10)'
-        }}
+            src={bannerImg}
+            alt="Banner Pokemon"
+            style={{
+            width: '100%',
+            maxWidth: '250px',
+            height: 'auto',
+            objectFit: 'contain',
+            borderRadius: '12px',
+            boxShadow: '0 4px 16px rgba(0,0,0,0.10)'
+            }}
         />
     </div>
 );
